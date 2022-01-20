28f883a363d6e6f65621ca2ab8ff1abd8315b639
slaven89 <kslav002@gmail.com>
2022-01-20 18:40:16 -0500

    saved player's choice to a variable in the function, not sure how much function nesting I need to use

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-var shoot = document.getElementById('shootButton');
-var playerChoice = ' ';
-var computerChoice = ' ';
+const shoot = document.getElementById('shootButton');
+const playerMenu = document.getElementById('choice');
+
+var playerChoice;
+var computerChoice;
+
+var playerScore = 0;
+var computerScore = 0;
+
 shoot.addEventListener('click' , playerShot);
 
 function playerShot (e) {
     e.preventDefault();
     shoot.disabled = true;
     console.log('the button clicked');
-
+    var playerChoice = playerMenu.value;
+    return playerChoice
 }
