ead2420a1866fcf1af673b2a893430a6233c8aba
slaven89 <kslav002@gmail.com>
2022-01-29 23:52:11 -0500

    implemented basic version of graphics for Rock Paper Scissors game

--- main.js
+++ main.js
@@ -7,12 +7,37 @@ var computerChoice;
 var playerScore = 0;
 var computerScore = 0;
 
+var playerVisual = document.getElementById('playerVisual');
+var computerVisual = document.getElementById('computerVisual');
+
 shoot.addEventListener('click' , playerShot);
 
 function playerShot (e) {
     e.preventDefault();
     shoot.disabled = true;
-    console.log('the button clicked');
     var playerChoice = playerMenu.value;
-    return playerChoice
+    var options = ['rock', 'paper', 'scissors'];
+    var rng = Math.floor(Math.random() * 3) + 1;
+    var rngTwo = rng - 1;
+    var computerChoice = options[rngTwo];
+    var results = [playerChoice, computerChoice];
+    console.log(results);
+        if (results[0] == 'Paper') {
+            playerVisual.src = '../January/images/playerpaper.jpg';
+    }
+        else if (results[0] == 'Scissors') {
+            playerVisual.src = '../January/images/playerscissors.jpg';
+        }
+        else {
+            playerVisual.src = '../January/images/playerrock.jpg';
+        }
+        if (results[1] == 'paper') {
+            computerVisual.src = '../January/images/computerpaper.jpeg';
+        }
+        else if (results[1] == 'rock') {
+            computerVisual.src = '../January/images/computerrock.jpg';
+        }
+        else {
+            computerVisual.src = '../January/images/computerscissors.jpg';
+        }
 }
